0bcdee4bda1fd117f1b76de2114e8041c6d60106
Martin Yoakum <MYOAKUM@WUSTL.EDU>
2020-10-12 04:58:41 -0500

    pullquotes

--- single.php
+++ single.php
@@ -25,12 +25,12 @@ get_header();
 					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'affl' ) . '</span> <span class="nav-title">%title</span>',
 				)
 			);
-			*/
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif;
+			*/
 
 		endwhile; // End of the loop.
 		?>
