cdd3fb2201b6ab063c0f0334928de8ecbbd4676b
Martin Yoakum <MYOAKUM@WUSTL.EDU>
2020-10-18 14:44:23 -0500

    Testimonials and Contact section animations

--- _assets/js/animations.js
+++ _assets/js/animations.js
@@ -31,45 +31,20 @@
 	});
 
 	/** Home About section **/
-	/*
-	aboutTimeline = new gsap.timeline({ defaults: { duration: 2, transformOrigin: "center", ease: "ease" } });
-	aboutLeftBracketTween.from( '#aboutLeftBracket', { duration: 2, x: '-200', opacity: 0 }, 0 )
-	aboutRightBracketTween.from( '#aboutRightBracket', { duration: 2, x: '200', opacity: 0 }, 0 )
-	aboutSummaaryTween.from( '#aboutus .summary', { duration: 2, y: '200', opacity: 0 }, 0 )
-	aboutBuzzOneTween.from( '#aboutus .slant__buzz-word:nth-child(1)', { duration: 2, x: '100', opacity: 0 }, 0 )
-	aboutBuzzTwoTween.from( '#aboutus .slant__buzz-word:nth-child(2)', { duration: 2, x: '-100', opacity: 0 }, 0 )
-	aboutBuzzThreeTween.from( '#aboutus .slant__buzz-word:nth-child(3)', { duration: 2, x: '100', opacity: 0 }, 0 );
-	*/
 	aboutLeftBracketTimeline = new gsap.timeline({ defaults: { duration: 2, transformOrigin: "center", ease: "ease" } });
-	aboutLeftBracketTimeline.from( '#aboutLeftBracket', { duration: 2, x: '-200', opacity: 0 }, 0 )
+	aboutLeftBracketTimeline.from( '#aboutLeftBracket', { duration: 2, x: '-200', opacity: 0 }, 0 );
 	aboutRightBracketTimeline = new gsap.timeline({ defaults: { duration: 2, transformOrigin: "center", ease: "ease" } });
-	aboutRightBracketTimeline.from( '#aboutRightBracket', { duration: 2, x: '200', opacity: 0 }, 0 )
+	aboutRightBracketTimeline.from( '#aboutRightBracket', { duration: 2, x: '200', opacity: 0 }, 0 );
 	aboutSummaryTimeline = new gsap.timeline({ defaults: { duration: 2, transformOrigin: "center", ease: "ease" } });
-	aboutSummaryTimeline.from( '#aboutus .summary', { duration: 2, y: '200', opacity: 0 }, 0 )
+	aboutSummaryTimeline.from( '#aboutus .summary', { duration: 2, y: '200', opacity: 0 }, 0 );
 	aboutBuzzOneTimeline = new gsap.timeline({ defaults: { duration: 2, transformOrigin: "center", ease: "ease" } });
-	aboutBuzzOneTimeline.from( '#aboutus .slant__buzz-word:nth-child(1)', { duration: 2, x: '100', opacity: 0 }, 0 )
+	aboutBuzzOneTimeline.from( '#aboutus .slant__buzz-word:nth-child(1)', { duration: 2, x: '100', opacity: 0 }, 0 );
 	aboutBuzzTwoTimeline = new gsap.timeline({ defaults: { duration: 2, transformOrigin: "center", ease: "ease" } });
-	aboutBuzzTwoTimeline.from( '#aboutus .slant__buzz-word:nth-child(2)', { duration: 2, x: '-100', opacity: 0 }, 0 )
+	aboutBuzzTwoTimeline.from( '#aboutus .slant__buzz-word:nth-child(2)', { duration: 2, x: '-100', opacity: 0 }, 0 );
 	aboutBuzzThreeTimeline = new gsap.timeline({ defaults: { duration: 2, transformOrigin: "center", ease: "ease" } });
 	aboutBuzzThreeTimeline.from( '#aboutus .slant__buzz-word:nth-child(3)', { duration: 2, x: '100', opacity: 0 }, 0 );
 
-
 	var controller = new ScrollMagic.Controller();
-	// Initialise ScrollMagic Scene
-/*
-	const aboutTween = new ScrollMagic.Scene({
-		triggerElement: "#aboutus",
-		triggerHook: 'onCenter',
-		duration: .5*aboutHeight
-	})
-	.setTween(aboutLeftBracketTween)
-	.setTween(aboutRightBracketTween)
-	.setTween(aboutSummaryTween)
-	.setTween(aboutBuzzOneTween)
-	.setTween(aboutBuzzTwoTween)
-	.setTween(aboutBuzzThreeTween)
-	.addTo(controller);
-*/
 	const aboutLeftBracketTween = new ScrollMagic.Scene({
 		triggerElement: "#aboutus",
 		triggerHook: 'onCenter',
@@ -114,41 +89,33 @@
 	.addTo(controller);
 
 
-
-
-
-
-
 	/** Home Services section **/
-	// var serviceImageTween=TweenMax.staggerFrom(selectAll(".service .service__image"),200,{x:-250,opacity:0},50),
-	// 	serviceTextTween=TweenMax.staggerFrom(selectAll(".service .service__copy"),200,{x:250,opacity:0},50)
 
-	/*
-	const serviceImageTween = gsap.from( '.service .service__image', { duration: .5, x: -250, opacity: 0, stagger: .5 });
-	const	serviceTextTween = gasp.from( '.service .service__copy', { duration: .5, x: 250, opacity: 0, stagger: .5 });
-	*/
-	/*
-	const serviceTextTween = gsap.from('.service .service__copy', {
-		duration: .5,
-		x: 250,
-		opacity: 0,
-		stagger: .5
-	});
-	*/
 
-	/*
-	serviceImagesScene=new ScrollMagic.Scene({triggerElement:"#services",duration:.75*servicesHeight}).setTween(serviceImageTween).addTo(controller),
-	serviceCopyScene=new ScrollMagic.Scene({triggerElement:"#services",duration:.75*servicesHeight}).setTween(serviceTextTween).addTo(controller),
-	*/
+	/** Home Testimonials section **/
+	testimonialsTimeline = new gsap.timeline({ defaults: { duration: 2, transformOrigin: "center", ease: "ease" } });
+	testimonialsTimeline.from( '.quotes__header', { duration: 2, y: '-250', opacity: 0 }, 0 );
 
-	/*
-	const serviceImageTween = new ScrollMagic.Scene({
-		triggerElement: "#services",
+	const testimonialsTween = new ScrollMagic.Scene({
+		triggerElement: "#testimonials",
 		triggerHook: 'onCenter',
-		duration: .75*servicesHeight
+		duration: 300
 	})
-	.setTween(serviceGsapTween)
+	.setTween(testimonialsTimeline)
 	.addTo(controller);
-	*/
+
+
+	/** Home Contact section **/
+	// contactNoteTween=TweenMax.from(document.querySelector(".contact__content"),1,{y:100,opacity:0})
+	contactTimeline = new gsap.timeline({ defaults: { duration: 2, transformOrigin: "center", ease: "ease" } });
+	contactTimeline.from( '.contact__content', { duration: 1, y: 100, opacity: 0 });
+
+	const contactTween = new ScrollMagic.Scene({
+		triggerElement: "#contact",
+		triggerHook: 'onCenter',
+		duration: 300
+	})
+	.setTween(contactTimeline)
+	.addTo(controller);
 
 }() );
